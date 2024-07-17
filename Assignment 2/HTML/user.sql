5c345751832498c1f583d4e2ff048cf3d6ad92fd
Rajasekar T <sayhellotoraji01@gmail.com>
2024-07-17 17:15:45 +0530

    Zoho Training Assignment 2

--- Assignment 2/HTML/user.sql
+++ Assignment 2/HTML/user.sql
@@ -1,5 +1,6 @@
 use user_details;
 
+drop table user_info;
 
 create table user_info(
     user_id INT NOT NULL AUTO_INCREMENT,
@@ -8,13 +9,14 @@ create table user_info(
     age INT NOT NULL,
     mobile_no CHAR(10) NOT NULL,
     gender CHAR(12) NOT NULL,
+    dob Date Not Null,
     login_password VARCHAR(24) NOT NULL,
     PRIMARY KEY(user_id)
 );
 
 
-INSERT INTO user_info(user_name, email, age, mobile_no, gender, login_password) 
-VALUES('Rajasekar T', 'raj@outlook.com', 27, '9999999999', 'MALE', 'JFSWD2');
+INSERT INTO user_info(user_name, email, age, mobile_no, gender, dob, login_password) 
+VALUES('Rajasekar T', 'raj@outlook.com', 27, '9999999999', 'MALE', '2024-02-24', 'JFSWD2');
 
 
 select * from user_info;
