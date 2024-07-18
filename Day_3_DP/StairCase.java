7092f32e452237a0e220644ec7adcb2c81690c9d
Rajasekar T <sayhellotoraji01@gmail.com>
2024-07-18 20:43:20 +0530

    Q5 - Staircase

---
+++ Day_3_DP/StairCase.java
@@ -0,0 +1,21 @@
+package Day_3_DP;
+
+import java.util.*;
+public class StairCase {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
